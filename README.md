bedf314d5348be2a7549dbabae0bb24b63fd1bdf
typos-r-us <kennethmwenda@gmail.com>
2021-10-20 13:05:07 +0300

    initial commit, empty flutter project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# libero_radio
+
+An AI Powered, Voice Assistant Android Radio App built in Flutter
+
+## Getting Started
---
+++ android/app/src/main/kotlin/com/kennethmwenda/libero_radio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kennethmwenda.libero_radio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
